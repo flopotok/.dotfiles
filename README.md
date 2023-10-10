735d3fb53618a5651029e56ef8c40a0fac0c7ac4
flopotok <florenciabsalinas@gmail.com>
2023-10-10 12:31:15 -0400

    port

---
+++ README.md
@@ -0,0 +1,14 @@
+# .dotfiles
+
+1. Clone this repository
+2. Run `install.sh`
+3. Open up new window to initiate `zsh` shell
+
+### Current issues
+
+- Would like to improve the install script
+- Handle installation on different OS (MacOS, Linux, WSL2)
+
+### Future optimizations
+
+- Improve install script by automating step 3 above, initializing the `zsh` environment
---
+++ install.sh
@@ -0,0 +1,19 @@
+brew install stow
+
+# stow dotfiles
+stow git
+stow tmux
+stow zsh
+
+# add zsh as a login shell
+command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell
+sudo chsh -s $(which zsh) $USER
+
+brew install antidote
+# bundle zsh plugins 
+antidote bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
+# Use kitty terminal on MacOS
+# [ `uname -s` = 'Darwin' ] && stow kitty
